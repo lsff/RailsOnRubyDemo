ba4a02609ab17856806e4b14996df1cbb1002842
liushifeng <shifeng_liu01@163.com>
2017-06-07 23:13:38 +0800

    Rails On Ruby Demo 学习记录

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rails On Ruby 初探
+
+代码来源：https://railstutorial-china.org/book/
+
+Push 到 Github 备忘！
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,31 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      if @user.activated?
+        # 登入用户，然后重定向到用户的资料页面
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or (@user)
+      else
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
+    else
+      # 创建一个错误消息
+      flash.now[:danger] = 'Invalid email/password combination' # 不完全正确
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+
+end
